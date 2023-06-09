d13a448c9740c8c8faeedc19b4d52e0583fc340b
Il Bonfo <stebon03@gmail.com>
2023-06-09 14:35:54 +0200

    Gen() grid

--- generatore.py
+++ generatore.py
@@ -27,7 +27,6 @@ def mask(grid: ListGrid, rate: float = 0.5) -> ListGrid:
     return grid
 
 
-
 def generate(mask_rate=0.5) -> ListGrid:
     attempt = 1
     while True:
@@ -54,4 +53,7 @@ def generate(mask_rate=0.5) -> ListGrid:
         logging.debug(f"generate by np_union attempt {attempt}")
     return mask(g_list, mask_rate)
 
-print(generate())
+
+grid = generate()
+for i in range(len(grid)):
+    print(grid[i])

1e223a36846a85edd1c1c53a0b18d8fde74d5226
Il Bonfo <stebon03@gmail.com>
2023-06-09 15:40:36 +0200

    Update Graphic.py

--- Graphic.py
+++ Graphic.py
@@ -1,16 +1,6 @@
 import pygame
 import copy
 
-grid = [[0, 0, 2, 0, 4, 0, 1, 9, 8],
-    [7, 0, 0, 0, 0, 0, 4, 5, 3],
-    [4, 1, 5, 8, 9, 0, 0, 0, 0],
-    [0, 2, 6, 7, 0, 0, 3, 4, 0],
-    [0, 4, 0, 0, 0, 2, 0, 1, 7],
-    [1, 0, 0, 0, 8, 0, 2, 6, 0],
-    [3, 8, 0, 0, 7, 0, 9, 2, 0],
-    [0, 6, 0, 0, 2, 8, 5, 3, 0],
-    [2, 0, 1, 0, 0, 0, 7, 8, 0]]
-
 # <editor-fold, Inizializzazione schermata e definizione costanti>
 pygame.init()
 pygame.display.set_caption("Sudoku")
@@ -76,14 +66,3 @@ def ins(window, pos, grid):
                         pass
     except IndexError:
         pass
-
-
-if __name__ == "__main__":
-    while True:
-        GUI(grid=grid)
-        for events in pygame.event.get():
-            if events.type == pygame.QUIT:
-                pygame.quit()
-            if events.type == pygame.MOUSEBUTTONUP:
-                pos = pygame.mouse.get_pos()
-                ins(window=win, pos=(pos[0]//50, pos[1]//50), grid=grid)
